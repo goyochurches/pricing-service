b7adcd3a2831e9f192fa69fd3af7fc08141d15c5
goyochurches <goyochurches@gmail.com>
2025-10-31 09:36:33 +0100

    refactor/ change class

--- src/main/java/pricing/domain/exception/ErrorResponse.java
+++ src/main/java/pricing/domain/exception/ErrorResponse.java
@@ -1,14 +1,10 @@
 package pricing.domain.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ErrorResponse {
 
     private LocalDateTime timestamp;
